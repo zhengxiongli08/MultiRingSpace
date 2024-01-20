442f55fea36b1d163ddc281cce434d5c393537e3
zhengxiongli08 <2518605604@qq.com>
2024-01-20 23:05:42 +0800

    Remove some useless files

--- match.py
+++ match.py
@@ -161,46 +161,6 @@ def TransformPoint(Point, TransMat):
     Point_Trans = np.squeeze(Point_Trans, axis=1)  # 删除指定维度为1的矩阵维度
     return Point_Trans
 
-# def GeometricConsistency(KeypointA, KeypointB):
-#     """
-#         基于几何差异，迭代出几何一致的成对的关键
-#     """
-#     # 输入:
-#     # KeypointA, KeypointB: Numpy数据格式,各行一一对应
-#     #
-#     # 初始化参数
-#     T_NumKP = 100                 # 控制最后一次成对关键点的数目
-#     T_IterativeResolution = 0.7  # 该值属于[0~1],取0与1非法,值越小迭代次数越少，结果越不稳定
-#     # Num_GoodKP = T_NumKP + 1   # 确保Num>T_NumKP首次执行满足条件
-#     Loop = 0                     # 统计迭代次数
-#     # 迭代
-#     E_Start = time.time()
-#     while True:
-#         # 遍历搜索，获得关键点之间的距离
-#         DistanceA, _ = NearKeypoint(KeypointA, KeypointA.shape[0], KeypointA)
-#         DistanceB, _ = NearKeypoint(KeypointB, KeypointB.shape[0], KeypointB)
-#         # 几何差异【图"间"距离筛选】【DD: Distance Difference】
-#         DDiff = np.abs(np.mean(DistanceA, axis=1) - np.mean(DistanceB, axis=1))
-#         # 找出T_IterativeResolution倍的成对关键，输出其索引
-#         Indexed_DDiffValue = [(Value, Index) for Index, Value in enumerate(DDiff)]  # 创建带有索引的元组列表
-#         Index_GoodDD = [Index for (Value, Index) in heapq.nsmallest(int(T_IterativeResolution*DDiff.shape[0]), Indexed_DDiffValue, key=lambda x: x[0])]
-#         # 更新迭代所需的数据
-#         KeypointA = KeypointA[Index_GoodDD]
-#         KeypointB = KeypointB[Index_GoodDD]
-#         # 关键点数目【KeypointA与KeypointB的关键点数目始终相同】
-#         Num_GoodKP = KeypointA.shape[0]
-#         Loop += 1  # 统计迭代次数
-#         # 判定是否结束迭代,并准备输出值
-#         KPS_A, KPS_B, Aff_M, Mask = EstimateAffineTransformation(KeypointA, KeypointB)
-#         if (Mask.all() == True) or (Num_GoodKP < T_NumKP):
-#             KeypointA, KeypointB = KPS_A, KPS_B  # 迭代结束后使用变换矩阵是为了防止迭代结束后可能存在误匹配，尽管这种情况极小
-#             Num_GoodKP = KeypointA.shape[0]
-#             break
-#     # 报告迭代信息
-#     E_End = time.time()
-#     print(f'全局迭代信息: 共计迭代{Loop}次,耗时{E_End-E_Start:.3f}秒，从暴力匹配中，迭代产生{Num_GoodKP}对关键点')
-#     return KeypointA, KeypointB, Aff_M
-
 def GeometricConsistency_Guide(KeypointA, KeypointB, Magnification):
     """
         基于几何差异，迭代出几何一致的成对的关键
@@ -437,78 +397,6 @@ def DoLocalMatching(KeypointA, EncodeA, TransMat, KeypointB, EncodeB, W_LocalReg
         MatchPointA, MatchPointB = LocalMatching(KeypointA, EncodeA, TransMat, KeypointB, EncodeB, W_LocalRegionSize)
     else:
         MatchPointB, MatchPointA = LocalMatching(KeypointB, EncodeB, cv2.invertAffineTransform(TransMat), KeypointA, EncodeA, W_LocalRegionSize)  # 逆变换与转置
-    return MatchPointA, MatchPointB
-
-def LocalMatching_Guide(PointA, EncodeA, TransMat, PointB, EncodeB, A_crop_values_match, B_crop_values_match, W_LocalRegionSize=0.2):
-    """
-        【专供引导匹配使用, 解决图像尺寸由正方形变成长方形时坐标偏差问题】
-        在局部区域内【局部区域是指映射坐标周围的区域，该区域由指定数目的点划定，与四象限和圆域无关】执行点匹配
-    """
-    # 输入:
-    # PointA, PointB: Numpy数据格式
-    # TransMat: 变换矩阵,PointA向PointB方向映射
-    # EncodeA, EncodeB: Numpy数据格式,分别是PointA,PointB的描述子向量
-    #
-    # 输出:
-    # MatchPointA, MatchPointB: Numpy数据格式,局部匹配结果,行向一一对应
-    #
-    # 初始化参数
-    # W_LocalRegionSize = 0.02       # 使用一定比例的点数目近似局部区域的尺寸，局部区域近似为目标区域的10%大小
-    Num_PointB = PointB.shape[0]   # 点数目
-    Num_NearPoint = int(W_LocalRegionSize*Num_PointB)  # 在每个映射坐标周围找出Num个最近的关键点
-    print(f'在局部匹配中，设置局部区域尺寸权重为{W_LocalRegionSize}时，使得每个映射坐标所在的局部区域包含{Num_NearPoint}个关键点')
-    #
-    # 转变关键点的坐标【从关键点少的向关键点多的方向映射坐标】
-    Point_Map = TransformPoint(PointA, TransMat)
-    # 修正图像由正方形变成长方形时导致的坐标偏差
-    # Point_Map = Point_Map - np.array([A_crop_values_match[1],A_crop_values_match[0]]) + np.array([B_crop_values_match[1],B_crop_values_match[0]])
-    # Point_Map = Point_Map + B_crop_values_match
-    # 搜索映射坐标周围的关键点索引
-    Distance, Index_PointB_Near = NearKeypoint(PointB, Num_NearPoint, Point_Map)  # Num_NearPoint
-    # ++映射坐标与其周围关键点的相似性
-    # S1: A[B]numpy高级索引: 使用B中的每一行作为索引从A中获取行
-    EncodeB_Near = EncodeB[Index_PointB_Near]
-    # S2: 最近邻相似性矩阵
-    Similarity_Around = np.linalg.norm(EncodeB_Near - EncodeA[:, np.newaxis, :], axis=2)
-    # 获得局部最相似索引
-    Index_Similar_Around = np.argmin(Similarity_Around, axis=1)
-    # 依据局部最相似，索引出与KPA粗匹配的KPB
-    Index_PointB_Around = Index_PointB_Near[range(Index_Similar_Around.shape[0]), Index_Similar_Around]
-    # 显示局部区域的粗匹配结果
-    # Draw.PointLine(PointA, PointB[Index_PointB_Around, SizeKps=10, LineWidth=0.1)
-    # 降重
-    Index_PointA, Index_PointB = RemoveRepetition_Local(Index_PointB_Around, Similarity_Around, Index_Similar_Around, np.arange(PointA.shape[0]))
-    PointA_NoRep = PointA[Index_PointA]
-    PointB_NoRep = PointB[Index_PointB]
-    print(f'局部粗匹配获得{Index_PointA.shape[0]}个成对的关键点,此时还未执行基于区域的几何一致性评估')
-    # 查看将重后局部匹配的粗匹配结果
-    # Draw.PointLine(PointA_NoRep, PointB_NoRep, SizeKps=10, LineWidth=1.5)
-    # 区域内成对的关键点
-    PA, PB = KeypintInRegion(PointA_NoRep, PointB_NoRep)
-    # 在划分区域的内分别进行几何一致性评估
-    PointA_MR, PointB_MR = GeometricConsistencyForRegion(PA['MR'], PB['MR'])
-    PointA_UR, PointB_UR = GeometricConsistencyForRegion(PA['UR'], PB['UR'])
-    PointA_DR, PointB_DR = GeometricConsistencyForRegion(PA['DR'], PB['DR'])
-    PointA_LR, PointB_LR = GeometricConsistencyForRegion(PA['LR'], PB['LR'])
-    PointA_RR, PointB_RR = GeometricConsistencyForRegion(PA['RR'], PB['RR'])
-    # 降重
-    MatchPointA = np.vstack((PointA_MR, PointA_UR, PointA_DR, PointA_LR, PointA_RR))
-    MatchPointB = np.vstack((PointB_MR, PointB_UR, PointB_DR, PointB_LR, PointB_RR))
-    MatchPointA, Index_NoRepKP = np.unique(MatchPointA, axis=0, return_index=True)
-    MatchPointB = MatchPointB[Index_NoRepKP]
-    print(f'局部精匹配获得{Index_NoRepKP.shape[0]}个成对的关键点,这是执行基于区域的几何一致性评估后的结果')
-    return MatchPointA, MatchPointB
-
-
-def DoLocalMatching_Guide(KeypointA, EncodeA, TransMat, KeypointB, EncodeB, A_crop_values_match, B_crop_values_match, W_LocalRegionSize):
-    """
-        执行局部匹配【专供引导匹配使用, 解决图像尺寸由正方形变成长方形时坐标偏差问题】
-    """
-    # 初始化数据【判断KeypointA和KeypointB的数目哪个更少】
-    if KeypointA.shape[0] <= KeypointB.shape[0]:
-        MatchPointA, MatchPointB = LocalMatching_Guide(KeypointA, EncodeA, TransMat, KeypointB, EncodeB, A_crop_values_match, B_crop_values_match, W_LocalRegionSize)
-    else:
-        MatchPointB, MatchPointA = LocalMatching_Guide(KeypointB, EncodeB, cv2.invertAffineTransform(TransMat), KeypointA, EncodeA, B_crop_values_match, A_crop_values_match, W_LocalRegionSize)  # 逆变换与转置
     return MatchPointA, MatchPointB
 
 def Matching_Origin(KeypointA, EncodeA, KeypointB, EncodeB, Magnification):
